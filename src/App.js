feaa963edd7878a64c05fdc239f5c0262bf46f3b
Pawel900 <pawel.wysoczanski01@gmail.com>
2020-01-17 08:37:08 +0100

    zmiana opisu

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I want to learn React
+          I'm learning React.
         </a>
       </header>
     </div>
